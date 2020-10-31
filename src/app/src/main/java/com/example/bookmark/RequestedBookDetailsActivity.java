c3abe8c13cfa36c96df5556bd4aabb99c370a588
ryankortbeek <rakortbe@ualberta.ca>
2020-10-31 13:49:33 -0600

    modified activities with a back button such that they extend from BackButtonActivity

--- src/app/src/main/java/com/example/bookmark/RequestedBookDetailsActivity.java
+++ src/app/src/main/java/com/example/bookmark/RequestedBookDetailsActivity.java
@@ -1,14 +1,13 @@
 package com.example.bookmark;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 /**
  * TODO: Description of class.
+ *
  * @author Nayan Prakash.
  */
-public class RequestedBookDetailsActivity extends AppCompatActivity {
+public class RequestedBookDetailsActivity extends BackButtonActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
