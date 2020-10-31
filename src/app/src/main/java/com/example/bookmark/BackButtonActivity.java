cc8383dc88c8306061715fca135e252a5d0edcb9
ryankortbeek <rakortbe@ualberta.ca>
2020-10-31 13:47:49 -0600

    implemented backward navigation functionality

--- src/app/src/main/java/com/example/bookmark/BackButtonActivity.java
+++ src/app/src/main/java/com/example/bookmark/BackButtonActivity.java
@@ -1,4 +1,22 @@
 package com.example.bookmark;
 
-public class BackButtonActivity {
+import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * This activity implements the backwards navigation functionality used by
+ * activities with a back button.
+ * TODO: verify back button functionality
+ *
+ * @author Ryan Kortbeek.
+ */
+public class BackButtonActivity extends AppCompatActivity {
+    @Override
+    public boolean onSupportNavigateUp() {
+        Intent intent = getIntent();
+        setResult(AppCompatActivity.RESULT_CANCELED, intent);
+        finish();
+        return true;
+    }
 }
