feafe47018e43132ad7ba7d88f5dfc0936cab323
Kyle Hennig <henn.kyle@outlook.com>
2020-11-02 23:25:41 -0700

    Add comments to methods of book class

--- src/app/src/main/java/com/example/bookmark/models/Book.java
+++ src/app/src/main/java/com/example/bookmark/models/Book.java
@@ -22,6 +22,14 @@ public class Book implements FirestoreSerializable {
     private String description = "";
     private Status status = Status.AVAILABLE;
 
+    /**
+     * Creates a Book.
+     *
+     * @param title  The title.
+     * @param author The author.
+     * @param isbn   This ISBN.
+     * @param owner  The username of the owner.
+     */
     public Book(String title, String author, String isbn, String owner) {
         this.title = title;
         this.author = author;
@@ -29,42 +37,92 @@ public class Book implements FirestoreSerializable {
         this.owner = owner;
     }
 
+    /**
+     * Gets the title.
+     *
+     * @return The title.
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Gets the author.
+     *
+     * @return The author.
+     */
     public String getAuthor() {
         return author;
     }
 
+    /**
+     * Gets the ISBN.
+     *
+     * @return The ISBN.
+     */
     public String getIsbn() {
         return isbn;
     }
 
+    /**
+     * Gets the username of the owner.
+     *
+     * @return The username of the owner.
+     */
     public String getOwner() {
         return owner;
     }
 
+    /**
+     * Gets the photograph.
+     *
+     * @return The photograph.
+     */
     public Photograph getPhotograph() {
         return photograph;
     }
 
+    /**
+     * Sets the photograph.
+     *
+     * @param photograph The photograph.
+     */
     public void setPhotograph(Photograph photograph) {
         this.photograph = photograph;
     }
 
+    /**
+     * Gets the description.
+     *
+     * @return The description.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Set the description.
+     *
+     * @param description The description.
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Gets the status.
+     *
+     * @return The status.
+     */
     public Status getStatus() {
         return status;
     }
 
+    /**
+     * Sets the status.
+     *
+     * @param status The status.
+     */
     public void setStatus(Status status) {
         this.status = status;
     }
