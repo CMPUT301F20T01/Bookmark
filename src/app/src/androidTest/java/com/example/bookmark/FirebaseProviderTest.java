3e2f7952ac3e5ac03b268319c61e84a632eb1bca
Kyle Hennig <henn.kyle@outlook.com>
2020-11-05 15:43:21 -0700

    Allow for deleting requests

--- src/app/src/androidTest/java/com/example/bookmark/FirebaseProviderTest.java
+++ src/app/src/androidTest/java/com/example/bookmark/FirebaseProviderTest.java
@@ -2,6 +2,7 @@ package com.example.bookmark;
 
 import com.example.bookmark.models.Book;
 import com.example.bookmark.models.Borrower;
+import com.example.bookmark.models.Request;
 import com.example.bookmark.models.User;
 import com.example.bookmark.server.FirebaseProvider;
 
@@ -25,10 +26,10 @@ public class FirebaseProviderTest {
                 assertEquals(user, user2);
                 semaphore.release();
             }, e -> {
-                fail("An error occured while getting the user.");
+                fail("An error occurred while getting the user.");
             });
         }, e -> {
-            fail("An error occured while creating the user.");
+            fail("An error occurred while creating the user.");
         });
         while (true) {
             try {
@@ -52,10 +53,10 @@ public class FirebaseProviderTest {
                 assertEquals(book, book2);
                 semaphore.release();
             }, e -> {
-                fail("An error occured while getting the book.");
+                fail("An error occurred while getting the book.");
             });
         }, e -> {
-            fail("An error occured while creating the book.");
+            fail("An error occurred while creating the book.");
         });
         while (true) {
             try {
@@ -68,23 +69,27 @@ public class FirebaseProviderTest {
     }
 
     /**
-     * Tests the creation of a book with a request.
+     * Tests the creation and deletion of a request.
      */
     @Test
-    public void testCreateAndGetBookWithRequest() {
+    public void testCreateAndGetAndDeleteRequest() {
         Semaphore semaphore = new Semaphore(0);
         Borrower borrower = new Borrower("mary.jane9", "Mary", "Jane", "mjane@ualberta.ca", "7809999999");
         Book book = new Book("Programming Pearls", "Jon Bentley", "978-0-201-65788-3", "john.smith42");
-        book.requestBook(borrower);
-        FirebaseProvider.getInstance().storeBook(book, aVoid -> {
-            FirebaseProvider.getInstance().retrieveBookByIsbn(book.getIsbn(), book2 -> {
-                assertEquals(book, book2);
-                semaphore.release();
+        Request request = new Request(book, borrower, null);
+        FirebaseProvider.getInstance().storeRequest(request, aVoid -> {
+            FirebaseProvider.getInstance().retrieveRequestByUserAndBook(borrower, book, request2 -> {
+                assertEquals(request, request2);
+                FirebaseProvider.getInstance().deleteRequest(request, aVoid2 -> {
+                    semaphore.release();
+                }, e -> {
+                    fail("An error occurred while deleting the request");
+                });
             }, e -> {
-                fail("An error occured while getting the book.");
+                fail("An error occurred while getting the request.");
             });
         }, e -> {
-            fail("An error occured while creating the book.");
+            fail("An error occurred while creating the request.");
         });
         while (true) {
             try {
