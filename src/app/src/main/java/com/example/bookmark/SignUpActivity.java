19f1cd14965e9b29b5ac9e25e0de8c02ca0838ca
konradstanski <kstanisz@ualberta.ca>
2020-11-10 02:40:18 -0700

    Refactor login/signup and add error dialogs

--- src/app/src/main/java/com/example/bookmark/SignUpActivity.java
+++ src/app/src/main/java/com/example/bookmark/SignUpActivity.java
@@ -5,14 +5,14 @@ import android.os.Bundle;
 import android.telephony.PhoneNumberFormattingTextWatcher;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 
 import com.example.bookmark.models.User;
 import com.example.bookmark.server.FirebaseProvider;
+import com.example.bookmark.util.DialogUtil;
+import com.example.bookmark.util.EmptyTextFocusListener;
 import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.ArrayList;
@@ -24,7 +24,6 @@ import static com.example.bookmark.util.UserInfoFormValidator.validateEditTextEm
 public class SignUpActivity extends BackButtonActivity {
     private EditText userNameEditText, firstNameEditText, lastNameEditText, emailAddressEditText, phoneNumberEditText;
     private TextInputLayout userNameLayout, firstNameLayout, lastNameLayout, emailAddressLayout, phoneNumberLayout;
-    private Button signUpButton;
 
 
     @Override
@@ -33,15 +32,13 @@ public class SignUpActivity extends BackButtonActivity {
         setContentView(R.layout.activity_signup);
         getSupportActionBar().setTitle("Sign Up");
 
-        FirebaseProvider firebaseProvider = FirebaseProvider.getInstance();
-
         // get relevant views
         userNameEditText = findViewById(R.id.signup_username_edit_text);
         firstNameEditText = findViewById(R.id.signup_firstname_edit_text);
         lastNameEditText = findViewById(R.id.signup_lastname_edit_text);
         emailAddressEditText = findViewById(R.id.signup_email_edit_text);
         phoneNumberEditText = findViewById(R.id.signup_phonenumber_edit_text);
-        signUpButton = findViewById(R.id.signup_signup_button);
+        Button signUpButton = findViewById(R.id.signup_signup_button);
 
         userNameLayout = findViewById(R.id.signup_username_textInputLayout);
         firstNameLayout = findViewById(R.id.signup_firstname_textInputLayout);
@@ -49,62 +46,10 @@ public class SignUpActivity extends BackButtonActivity {
         emailAddressLayout = findViewById(R.id.signup_email_textInputLayout);
         phoneNumberLayout = findViewById(R.id.signup_phonenumber_textInputLayout);
 
-        ArrayList<EditText> formTexts = new ArrayList<>();
-        formTexts.add(userNameEditText);
-        formTexts.add(firstNameEditText);
-        formTexts.add(lastNameEditText);
-
-        ArrayList<TextInputLayout> formLayouts = new ArrayList<>();
-        formLayouts.add(userNameLayout);
-        formLayouts.add(firstNameLayout);
-        formLayouts.add(lastNameLayout);
-
-        signUpButton.setOnClickListener(view -> {
-            boolean invalidField = false;
-            // double check all form fields filled
-            for (int i = 0; i < formTexts.size(); i++) {
-                if (!validateEditTextEmpty(formTexts.get(i), formLayouts.get(i))) {
-                    invalidField = true;
-                }
-            }
-            if (!checkIfEditTextValidEmail(emailAddressEditText, emailAddressLayout)) {
-                invalidField = true;
-            }
-            if (!checkIfEditTextValidPhoneNumber(phoneNumberEditText, phoneNumberLayout)) {
-                invalidField = true;
-            }
-            if (invalidField) {
-                return;
-            }
-
-            // check username is not in db already
-            String username = userNameEditText.getText().toString();
-            firebaseProvider.retrieveUserByUsername(username,
-                user -> {
-                    if (user == null) {
-                        // Get values from fields and create user object
-                        String firstName = firstNameEditText.getText().toString();
-                        String lastName = lastNameEditText.getText().toString();
-                        String emailAddress = emailAddressEditText.getText().toString();
-                        String phoneNumber = phoneNumberEditText.getText().toString();
-
-                        // add user and go back to main activity
-                        User newUser = new User(username, firstName, lastName, emailAddress, phoneNumber);
-                        firebaseProvider.storeUser(newUser,
-                            aVoid -> {
-                                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
-                                intent.putExtra("SIGNED_UP_USERNAME", username);
-                                startActivity(intent);
-                            },
-                            e -> Toast.makeText(SignUpActivity.this, "Connection Error. Please Try again.", Toast.LENGTH_LONG).show());
-                    } else {
-                        userNameLayout.setError("Username already registered. Please choose another");
-                    }
-                },
-                e -> Toast.makeText(SignUpActivity.this, "Connection Error. Please Try again.", Toast.LENGTH_LONG).show());
-        });
+        signUpButton.setOnClickListener(v -> validateAndAddUser());
 
         // allows user to see if their email address is valid as they type
+        phoneNumberEditText.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
         phoneNumberEditText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -120,7 +65,6 @@ public class SignUpActivity extends BackButtonActivity {
             }
         });
 
-        phoneNumberEditText.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
 
         // allows user to see if their phone number is valid as they type
         emailAddressEditText.addTextChangedListener(new TextWatcher() {
@@ -139,11 +83,9 @@ public class SignUpActivity extends BackButtonActivity {
         });
 
         // focus change listeners
-        for (int i = 0; i < formTexts.size(); i++) {
-            EditText text = formTexts.get(i);
-            TextInputLayout layout = formLayouts.get(i);
-            formTexts.get(i).setOnFocusChangeListener(new EmptyTextFocusListener(text, layout));
-        }
+        userNameEditText.setOnFocusChangeListener(new EmptyTextFocusListener(userNameEditText, userNameLayout));
+        firstNameEditText.setOnFocusChangeListener(new EmptyTextFocusListener(firstNameEditText, firstNameLayout));
+        lastNameEditText.setOnFocusChangeListener(new EmptyTextFocusListener(lastNameEditText, lastNameLayout));
 
         emailAddressEditText.setOnFocusChangeListener((v, hasFocus) -> {
             if (hasFocus) {
@@ -169,26 +111,57 @@ public class SignUpActivity extends BackButtonActivity {
             }
         });
     }
-}
 
-/**
- * Custom OnFocusChangeListener to eliminate super repetitive listener
- * declarations for all "empty text" checking situations.
- */
-class EmptyTextFocusListener implements View.OnFocusChangeListener {
-    EditText text;
-    TextInputLayout layout;
-
-    public EmptyTextFocusListener(EditText text, TextInputLayout layout) {
-        this.text = text;
-        this.layout = layout;
-    }
-
-    public void onFocusChange(View v, boolean hasFocus) {
-        if (hasFocus) {
-            this.layout.setError(null);
-        } else {
-            validateEditTextEmpty(this.text, this.layout);
+    private void validateAndAddUser() {
+        boolean invalidField = false;
+        // double check all form fields filled
+        if (!validateEditTextEmpty(userNameEditText, userNameLayout)) {
+            invalidField = true;
         }
+        if (!validateEditTextEmpty(firstNameEditText, firstNameLayout)) {
+            invalidField = true;
+        }
+        if (!validateEditTextEmpty(lastNameEditText, lastNameLayout)) {
+            invalidField = true;
+        }
+        if (!checkIfEditTextValidEmail(emailAddressEditText, emailAddressLayout)) {
+            invalidField = true;
+        }
+        if (!checkIfEditTextValidPhoneNumber(phoneNumberEditText, phoneNumberLayout)) {
+            invalidField = true;
+        }
+        if (invalidField) {
+            return;
+        }
+
+        // check username is not in db already
+        String username = userNameEditText.getText().toString();
+        FirebaseProvider.getInstance().retrieveUserByUsername(username,
+            user -> {
+                if (user == null) {
+                    // Get values from fields and create user object
+                    String firstName = firstNameEditText.getText().toString();
+                    String lastName = lastNameEditText.getText().toString();
+                    String emailAddress = emailAddressEditText.getText().toString();
+                    String phoneNumber = phoneNumberEditText.getText().toString();
+
+                    // add user and go back to main activity
+                    User newUser = new User(username, firstName, lastName, emailAddress, phoneNumber);
+                    FirebaseProvider.getInstance().storeUser(newUser,
+                        aVoid -> {
+                            Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                            intent.putExtra("SIGNED_UP_USERNAME", username);
+                            startActivity(intent);
+                        },
+                        e -> DialogUtil.showErrorDialog(this,
+                            new Exception("Connection Error. Please Try again."))
+                    );
+                } else {
+                    userNameLayout.setError("Username already registered. Please choose another");
+                }
+            },
+            e -> DialogUtil.showErrorDialog(this,
+                new Exception("Connection Error. Please Try again."))
+        );
     }
 }
