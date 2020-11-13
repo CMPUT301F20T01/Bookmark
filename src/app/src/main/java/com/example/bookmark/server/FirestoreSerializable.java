3e8a4369abd4950bb150ed804de4f1395cf35dcf
Kyle Hennig <henn.kyle@outlook.com>
2020-11-12 17:42:51 -0700

    Add missing details to class Javadoc comments

--- src/app/src/main/java/com/example/bookmark/server/FirestoreSerializable.java
+++ src/app/src/main/java/com/example/bookmark/server/FirestoreSerializable.java
@@ -2,6 +2,11 @@ package com.example.bookmark.server;
 
 import java.util.Map;
 
+/**
+ * Interface for an object that can be serialized to a Firebase compatible format.
+ *
+ * @author Kyle Hennig.
+ */
 public interface FirestoreSerializable {
     /**
      * Gets the object as a map of strings to types supported by Firestore documents.
--- src/app/src/main/java/com/example/bookmark/server/StorageServiceProvider.java
+++ src/app/src/main/java/com/example/bookmark/server/StorageServiceProvider.java
@@ -4,6 +4,8 @@ import android.util.Log;
 
 /**
  * A provider for the app's storage service.
+ *
+ * @author Kyle Hennig.
  */
 public class StorageServiceProvider {
     private static final String TAG = "StorageServiceProvider";
