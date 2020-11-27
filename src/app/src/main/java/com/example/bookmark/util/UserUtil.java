f70f66c1377a1c0c94348329a94bebd4ce383ecc
konradstanski <kstanisz@ualberta.ca>
2020-11-27 02:42:08 -0700

    Move setting logged in user to UserUtil

--- src/app/src/main/java/com/example/bookmark/util/UserUtil.java
+++ src/app/src/main/java/com/example/bookmark/util/UserUtil.java
@@ -3,9 +3,11 @@ package com.example.bookmark.util;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import static android.content.Context.MODE_PRIVATE;
+
 public class UserUtil {
     public static String getLoggedInUser(Context context) {
-        SharedPreferences sharedPreferences = context.getSharedPreferences("LOGGED_IN_USER", Context.MODE_PRIVATE);
+        SharedPreferences sharedPreferences = context.getSharedPreferences("LOGGED_IN_USER", MODE_PRIVATE);
         String username = sharedPreferences.getString("USER_NAME", "ERROR_NO_USER");
         if (username.equals("ERROR_NO_USER")) {
             DialogUtil.showErrorDialog(context, new Exception(username));
@@ -13,5 +15,12 @@ public class UserUtil {
         } else {
             return username;
         }
+    }
+
+    public static void setLoggedInUser(Context context, String username) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("LOGGED_IN_USER", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("USER_NAME", username);
+        editor.commit();
     }
 }
