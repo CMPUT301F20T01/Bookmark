2014bd09535b09f69e0e852138cd0b90a83d3a10
konradstanski <kstanisz@ualberta.ca>
2020-11-27 11:00:44 -0700

    Typo fixing in userUtil

--- src/app/src/main/java/com/example/bookmark/util/UserUtil.java
+++ src/app/src/main/java/com/example/bookmark/util/UserUtil.java
@@ -6,13 +6,13 @@ import android.content.SharedPreferences;
 import static android.content.Context.MODE_PRIVATE;
 
 /**
- * This class implements utility funcitons to set and retrieve the logged in user
+ * This class implements utility functions to set and retrieve the logged in user
  *
  * @author Kyle Hennig, Konrad Staniszewski
  */
 public class UserUtil {
     public static String getLoggedInUser(Context context) {
-        SharedPreferences sharedPreferences = context.getSharedPreferences("LOGGED_IN_USER", MODE_PRIVATE);
+        SharedPreferences sharedPreferences = context.getSharedPreferences("LOGGED_IN_USER", Context.MODE_PRIVATE);
         String username = sharedPreferences.getString("USER_NAME", "ERROR_NO_USER");
         if (username.equals("ERROR_NO_USER")) {
             DialogUtil.showErrorDialog(context, new Exception(username));
@@ -23,7 +23,7 @@ public class UserUtil {
     }
 
     public static void setLoggedInUser(Context context, String username) {
-        SharedPreferences sharedPreferences = context.getSharedPreferences("LOGGED_IN_USER", MODE_PRIVATE);
+        SharedPreferences sharedPreferences = context.getSharedPreferences("LOGGED_IN_USER", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString("USER_NAME", username);
         editor.commit();
