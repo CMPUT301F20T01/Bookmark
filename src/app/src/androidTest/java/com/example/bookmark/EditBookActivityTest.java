97f9f39baaab70855f1e4b23d24fc2cd22296cdc
Mitchell Adam <mbadam@ualberta.ca>
2020-11-27 14:08:50 -0800

    Add tests, all but editActivity should pass

--- src/app/src/androidTest/java/com/example/bookmark/EditBookActivityTest.java
+++ src/app/src/androidTest/java/com/example/bookmark/EditBookActivityTest.java
@@ -1,7 +1,9 @@
 package com.example.bookmark;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
@@ -10,6 +12,7 @@ import com.example.bookmark.mocks.MockStorageService;
 import com.example.bookmark.models.Book;
 import com.example.bookmark.models.User;
 import com.example.bookmark.server.StorageServiceProvider;
+import com.google.android.material.textfield.TextInputLayout;
 import com.robotium.solo.Solo;
 
 import org.junit.After;
@@ -21,40 +24,45 @@ import org.junit.Test;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
+import static android.content.Context.MODE_PRIVATE;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
+
 /**
  * Perform intent testing on the EditBookActivity
- * <p>
- * Outstanding Issues/TODOs
- * Test Data entry
- * Test successfully edited book to correct user
- * Test delete book
- * Test delete photo
- * Test add photo
  *
  * @author Mitch Adam.
  */
 public class EditBookActivityTest {
     private Solo solo;
-
-    @BeforeClass
-    public static void setUpOnce() {
-        StorageServiceProvider.setStorageService(MockStorageService.getMockStorageService());
-    }
+    private Book testBook;
 
     @Rule
     public ActivityTestRule<EditBookActivity> rule =
         new ActivityTestRule<EditBookActivity>(EditBookActivity.class, true, true) {
             @Override
             protected Intent getActivityIntent() {
-                User owner = MockModels.getMockOwner();
-                Book book = new Book(owner, "TEST", "TEST", "TEST");
+                testBook = MockModels.getMockBook3();
                 Intent intent = new Intent();
                 Bundle bundle = new Bundle();
-                bundle.putSerializable("Book", book);
+                bundle.putSerializable("Book", testBook);
                 intent.putExtras(bundle);
                 return intent;
             }
         };
+
+    /**
+     * Set Storage provider and current user
+     */
+    @BeforeClass
+    public static void setUpOnce() {
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        SharedPreferences sharedPreferences = context.getSharedPreferences("LOGGED_IN_USER", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("USER_NAME", "john.smith42").commit();
+        StorageServiceProvider.setStorageService(MockStorageService.getMockStorageService());
+    }
 
     /**
      * Runs before all tests and creates solo instance.
@@ -65,7 +73,6 @@ public class EditBookActivityTest {
     public void setUp() throws Exception {
         solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
     }
-
 
     /**
      * Gets the Activity
@@ -82,10 +89,59 @@ public class EditBookActivityTest {
      * Test opening Scan ISBN activity
      */
     @Test
-    public void scanISBN() {
+    public void openScanISBN() {
         // Image 0 is the camera icon in the ISBN text field
         solo.clickOnImageButton(0);
         solo.assertCurrentActivity("WRONG ACTIVITY", ScanIsbnActivity.class);
+    }
+
+    /**
+     * Test Editing a book
+     */
+    @Test
+    public void editBook() {
+        final String title = "Edited Title";
+        final String author = "Edited Author";
+        final String description = "Edited description";
+
+        solo.clearEditText(((TextInputLayout) solo.getView(R.id.add_edit_book_title)).getEditText());
+        solo.clearEditText(((TextInputLayout) solo.getView(R.id.add_edit_book_author)).getEditText());
+        solo.clearEditText(((TextInputLayout) solo.getView(R.id.add_edit_book_description)).getEditText());
+        solo.enterText(((TextInputLayout) solo.getView(R.id.add_edit_book_title)).getEditText(),
+            title);
+        solo.enterText(((TextInputLayout) solo.getView(R.id.add_edit_book_author)).getEditText(),
+            author);
+        solo.enterText(((TextInputLayout) solo.getView(R.id.add_edit_book_description)).getEditText(),
+            description);
+
+        solo.clickOnButton("DONE");
+        User owner = MockModels.getMockOwner();
+        StorageServiceProvider.getStorageService().retrieveBook(owner, testBook.getIsbn(), book -> {
+            assertEquals(title, book.getTitle());
+            assertEquals(author, book.getAuthor());
+            assertEquals(description, book.getDescription());
+        }, e -> fail("An error occurred while retrieving the book."));
+    }
+
+    /**
+     * Test Back Button does not edit book
+     */
+    public void doNotEditBook() {
+    }
+
+    /**
+     * Test Delete Book
+     */
+    @Test
+    public void deleteBook() {
+
+        solo.clickOnButton("Delete");
+        User owner = MockModels.getMockOwner();
+        StorageServiceProvider.getStorageService().retrieveBook(owner, testBook.getIsbn(), book2 ->
+                StorageServiceProvider.getStorageService().storeBook(testBook, aVoid2 -> {
+                    assertNull(book2);
+                }, e -> fail("An error occurred while recreating the deleted book.")),
+            e -> fail("An error occurred while retrieving the deleted book."));
     }
 
     /**
