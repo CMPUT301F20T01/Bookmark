4414c1c19e9c1f56ce0fc97e8a14f47e40c6ad7c
ryankortbeek <rakortbe@ualberta.ca>
2020-11-30 12:01:08 -0700

    finished tests for BorrowedActivity, ExploreActivity, PendingRequestsActivity

--- src/app/src/androidTest/java/com/example/bookmark/PendingRequestsActivityTest.java
+++ src/app/src/androidTest/java/com/example/bookmark/PendingRequestsActivityTest.java
@@ -3,13 +3,20 @@ package com.example.bookmark;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.view.View;
+import android.widget.ListView;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
+import com.example.bookmark.mocks.MockModels;
 import com.example.bookmark.mocks.MockStorageService;
+import com.example.bookmark.models.Book;
+import com.example.bookmark.models.User;
 import com.example.bookmark.server.StorageServiceProvider;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.robotium.solo.Solo;
 
 import org.junit.After;
@@ -19,9 +26,13 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static android.content.Context.MODE_PRIVATE;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 
 /**
@@ -83,37 +94,37 @@ public class PendingRequestsActivityTest {
     }
 
     /**
-     * Tests search functionality.
+     * Tests search functionality. Specifically the TextWatcher,
+     * toggleSearchVisibility(), updateAdapterFilter(), etc.
      */
-    // @Test
-    // public void search() {
-    //     View searchBtn =
-    //         rule.getActivity().findViewById(R.id.menu_filter_search_search_btn);
-    //     TextInputLayout searchBarLayout =
-    //         rule.getActivity().findViewById(R.id.search_bar_textInputLayout);
-    //     assertEquals(View.GONE, searchBarLayout.getVisibility());
-    //     solo.clickOnView(searchBtn);
-    //     solo.waitForText("Search");
-    //     assertEquals(View.VISIBLE, searchBarLayout.getVisibility());
-    //     TextInputEditText searchEditText =
-    //         rule.getActivity().findViewById(R.id.search_bar_textInput);
-    //     ListView listView =
-    //         rule.getActivity().findViewById(R.id.visible_books_listview);
-    //     Book mock1 = MockModels.getMockBook1();
-    //     mock1.setStatus(Book.Status.REQUESTED);
-    //     mock1.setDescription("Learning to code!");
-    //     Book mock2 = MockModels.getMockBook2();
-    //     mock2.setStatus(Book.Status.REQUESTED);
-    //     mock2.setDescription("LEARNT BEHAVIOUR...");
-    //     // TODO call listView.getAdapter().notifyDataSetChanged() here
-    //     solo.enterText(searchEditText, "lEaRn");
-    //     assertTrue(solo.searchText("Code Complete 2"));
-    //     assertTrue(solo.searchText("Programming Pearls"));
-    //     assertEquals(2, listView.getCount());
-    //     solo.enterText(searchEditText, " behaviour...");
-    //     assertTrue(solo.searchText("Programming Pearls"));
-    //     assertEquals(1, listView.getCount());
-    // }
+    @Test
+    public void search() {
+        View searchBtn =
+            rule.getActivity().findViewById(R.id.menu_filter_search_search_btn);
+        TextInputLayout searchBarLayout =
+            rule.getActivity().findViewById(R.id.search_bar_textInputLayout);
+        assertEquals(View.GONE, searchBarLayout.getVisibility());
+        solo.clickOnView(searchBtn);
+        solo.waitForText("Search");
+        assertEquals(View.VISIBLE, searchBarLayout.getVisibility());
+        TextInputEditText searchEditText =
+            rule.getActivity().findViewById(R.id.search_bar_textInput);
+        ListView listView =
+            rule.getActivity().findViewById(R.id.books_listview);
+
+        // Changes description of mock books
+        MockModels.getMockBook1().setDescription("Learning to code!");
+        MockModels.getMockBook2().setDescription("LEARNT BEHAVIOUR...");
+        MockModels.getMockBook5().setDescription("Trying to LeArN.");
+
+        solo.enterText(searchEditText, "lEaRn");
+        assertTrue(solo.searchText("Code Complete 2"));
+        assertTrue(solo.searchText("Programming Pearls"));
+        assertEquals(3, listView.getCount());
+        solo.enterText(searchEditText, " behaviour...");
+        assertTrue(solo.searchText("Programming Pearls"));
+        assertEquals(1, listView.getCount());
+    }
 
     /**
      * Ensures activity title is "Pending Requests". Tests getActivityTitle().
@@ -125,85 +136,109 @@ public class PendingRequestsActivityTest {
     }
 
     /**
-     * Ensures "Owner:" of each book is visible. Tests getBookOwnerVisibility().
+     * Ensures "Owner:" of each book is visible.
      */
-    // @Test
-    // public void checkBookOwnerVisibility() {
-    //     View searchBtn =
-    //         rule.getActivity().findViewById(R.id.menu_filter_search_search_btn);
-    //     solo.clickOnView(searchBtn);
-    //     TextInputEditText searchEditText =
-    //         rule.getActivity().findViewById(R.id.search_bar_textInput);
-    //     Book mock1 = MockModels.getMockBook1();
-    //     mock1.setDescription("Testing visibility of book attributes.");
-    //     solo.enterText(searchEditText, "Testing visibility of book attributes.");
-    //     assertTrue(solo.searchText("Owner:"));
-    // }
+    @Test
+    public void checkBookOwnerVisibility() {
+        View searchBtn =
+            rule.getActivity().findViewById(R.id.menu_filter_search_search_btn);
+        solo.clickOnView(searchBtn);
+        TextInputEditText searchEditText =
+            rule.getActivity().findViewById(R.id.search_bar_textInput);
+        MockModels.getMockBook1().setDescription("Testing visibility of book" +
+            " attributes.");
+        solo.enterText(searchEditText, "Testing visibility of book attributes.");
+        assertTrue(solo.searchText("Owner:"));
+    }
 
     /**
-     * Ensures "Status:" of each book is visible. Tests
-     * getBookStatusVisibility().
+     * Ensures "Status:" of each book is visible.
      */
-    // @Test
-    // public void checkBookStatusVisibility() {
-    //     View searchBtn =
-    //         rule.getActivity().findViewById(R.id.menu_filter_search_search_btn);
-    //     solo.clickOnView(searchBtn);
-    //     TextInputEditText searchEditText =
-    //         (TextInputEditText) solo.getView(R.id.search_bar_textInput);
-    //     Book mock1 = MockModels.getMockBook1();
-    //     mock1.setDescription("Testing visibility of book attributes.");
-    //     solo.enterText(searchEditText, "Testing visibility of book attributes.");
-    //     assertTrue(solo.searchText("Status:"));
-    // }
+    @Test
+    public void checkBookStatusVisibility() {
+        View searchBtn =
+            rule.getActivity().findViewById(R.id.menu_filter_search_search_btn);
+        solo.clickOnView(searchBtn);
+        TextInputEditText searchEditText =
+            (TextInputEditText) solo.getView(R.id.search_bar_textInput);
+        MockModels.getMockBook1().setDescription("Testing visibility of book" +
+            " attributes.");
+        solo.enterText(searchEditText, "Testing visibility of book " +
+            "attributes.");
+        assertTrue(solo.searchText("Status:"));
+    }
 
     /**
      * Check the count of displayed books. In doing so, tests the
      * getRelevantBooks() method of PendingRequestsActivity and the getBooks()
-     * method of ListingBooksActivity.
+     * and updateBookList() method of ListingBooksActivity.
      */
-    // @Test
-    // public void numberOfBooks() {
-    //     ListView listView = (ListView) solo.getView(R.id.visible_books_listview);
-    //     List<Book> validBooks = new ArrayList<>();
-    //     MockModels.getMockBook1().setStatus(Book.Status.ACCEPTED);
-    //     MockModels.getMockBook2().setStatus(Book.Status.REQUESTED);
-    //     // TODO call listView.getAdapter().notifyDataSetChanged() here
-    //     User user = MockModels.getMockRequester();
-    //     StorageServiceProvider.getStorageService().retrieveBooksByRequester(user,
-    //         books -> {
-    //             for (Book book : books) {
-    //                 if ((book.getStatus() == Book.Status.REQUESTED) ||
-    //                     (book.getStatus() == Book.Status.ACCEPTED)) {
-    //                     validBooks.add(book);
-    //                 }
-    //             }
-    //             assertEquals(validBooks.size(), listView.getCount());
-    //         }, e -> fail("An error occurred while retrieving the books by " +
-    //             "requester.")
-    //     );
-    // }
+    @Test
+    public void numberOfBooks() {
+        ListView listView = (ListView) solo.getView(R.id.books_listview);
+        List<Book> validBooks = new ArrayList<>();
+        User user = MockModels.getMockRequester();
+        StorageServiceProvider.getStorageService().retrieveBooksByRequester(user,
+            books -> {
+                for (Book book : books) {
+                    if ((book.getStatus() == Book.Status.REQUESTED) ||
+                        (book.getStatus() == Book.Status.ACCEPTED)) {
+                        validBooks.add(book);
+                    }
+                }
+                assertEquals(validBooks.size(), listView.getCount());
+            }, e -> fail("An error occurred while retrieving the books by " +
+                "requester.")
+        );
+    }
 
     /**
-     * Ensures that clicking on a book takes the user to the
-     * appropriate activity. In doing so, tests getPackageContext() and
+     * Ensures that clicking on a REQUESTED book takes the user to the
+     * appropriate activity. In doing so, tests getPackageContext(),
+     * goToBookDetails() and getIntentDestination().
+     */
+    @Test
+    public void goToBorrowerBookDetailsForRequested() {
+        View searchBtn =
+            rule.getActivity().findViewById(R.id.menu_filter_search_search_btn);
+        solo.clickOnView(searchBtn);
+        TextInputEditText searchEditText =
+            (TextInputEditText) solo.getView(R.id.search_bar_textInput);
+
+        MockModels.getMockBook1().setDescription("This book is requested.");
+        solo.enterText(searchEditText, "This book is requested.");
+        solo.clickInList(0, 0);
+        solo.assertCurrentActivity("WRONG ACTIVITY", BorrowerBookDetailsActivity.class);
+    }
+
+    /**
+     * Ensures that clicking on an ACCEPTED book takes the user to the
+     * appropriate activity. In doing so, tests getPackageContext(),
+     * goToBookDetails() and getIntentDestination().
+     */
+    @Test
+    public void goToBorrowerBookDetailsForAccepted() {
+        View searchBtn =
+            rule.getActivity().findViewById(R.id.menu_filter_search_search_btn);
+        solo.clickOnView(searchBtn);
+        TextInputEditText searchEditText =
+            (TextInputEditText) solo.getView(R.id.search_bar_textInput);
+
+        MockModels.getMockBook5().setDescription("This book is accepted.");
+        solo.enterText(searchEditText, "This book is accepted.");
+        solo.clickInList(0, 0);
+        solo.assertCurrentActivity("WRONG ACTIVITY", BorrowerBookDetailsActivity.class);
+    }
+
+    /**
+     * Ensures that the intent destination is set correctly. Tests
      * getIntentDestination().
      */
-    // @Test
-    // public void goToBorrowerBookDetails() {
-    //     View searchBtn =
-    //         rule.getActivity().findViewById(R.id.menu_filter_search_search_btn);
-    //     solo.clickOnView(searchBtn);
-    //     TextInputEditText searchEditText =
-    //         (TextInputEditText) solo.getView(R.id.search_bar_textInput);
-    //     Book mock1 = MockModels.getMockBook1();
-    //     mock1.setDescription("This book is requested.");
-    //     mock1.setStatus(Book.Status.REQUESTED);
-    //     // TODO call listView.getAdapter().notifyDataSetChanged() here
-    //     solo.enterText(searchEditText, "This book is requested.");
-    //     solo.clickInList(0, 0);
-    //     solo.assertCurrentActivity("WRONG ACTIVITY", BorrowerBookDetailsActivity.class);
-    // }
+    @Test
+    public void checkGetIntentDestination() {
+        assertEquals(BorrowerBookDetailsActivity.class,
+            rule.getActivity().getIntentDestination());
+    }
 
     /**
      * Close activity after each test.
